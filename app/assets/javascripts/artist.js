b71713de680fa1a29f51cde1d4d62fd180d97bba
Kang0 <kevinkangg@gmail.com>
2019-05-13 18:01:15 -0400

    added toggle button to tracks for album search results

--- app/assets/javascripts/artist.js
+++ app/assets/javascripts/artist.js
@@ -1,3 +1,12 @@
-$(function {
-    
-})
+$(function () {
+    console.log('artist.js is loaded ...')
+    toggleTracks()
+});
+
+function toggleTracks() {
+    $('button#toggleTrack').on('click', function(e) {
+        let id = $(this).data("id")
+        let idSelect = "ul#" + id
+        $(idSelect).toggle();
+    })
+}
