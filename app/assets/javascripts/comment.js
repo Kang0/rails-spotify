c69838dde548a8d91eff389cdfe210aa3c8b1891
Kang0 <kevinkangg@gmail.com>
2019-05-14 19:36:06 -0400

    added user name jquery comment show

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,15 +1,16 @@
 $(function () {
     console.log('comment.js is loaded ...')
-    showComment()
+    showNewComment()
 });
 
-function showComment() {
+function showNewComment() {
     $('form').submit(function(event) {
         event.preventDefault()
         let comment_values = $(this).serialize()
         let comment_post = $.post('/comments', comment_values)
 
         comment_post.done(function(data) {
+            $("#commentUser").text(data["user"]["name"])
             $("#commentContent").text(data["content"])
             console.log(data)
             $('form')[0].reset()
