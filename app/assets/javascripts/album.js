db7f34587d4422639edefc10103fad8d95bd1816
Kang0 <kevinkangg@gmail.com>
2019-05-15 11:54:38 -0400

    added data attributes to show full review button

--- app/assets/javascripts/album.js
+++ app/assets/javascripts/album.js
@@ -1,4 +1,10 @@
 $(function () {
     console.log('album.js is loaded ...')
-    
+    showFullReview()
 });
+
+function showFullReview() {
+    $("#fullReview").on("click", function () {
+        
+    })
+}
